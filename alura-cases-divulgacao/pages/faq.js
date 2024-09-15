e8c0704f77a2c2e0c7457a9ac3dd8dbfb2b62837
Marco <mbarozzi@gmail.com>
2024-09-15 14:35:52 -0300

    exemplificando o uso de getStaticProps

--- alura-cases-divulgacao/pages/faq.js
+++ alura-cases-divulgacao/pages/faq.js
@@ -1,29 +1,43 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "../scr/components/Link";
 
+export async function getStaticProps(context) {
+  return {
+    props: {
+      texto: 'ainda esta em carga'
+    },
+  };
+}
 
+export default function FaqPage(props) {
 
-export default function FaqPage() {
-  const [faq,setFaq]= useState([]);
-  useEffect(()=>{
-    const FAQ_API_URL ='https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+  console.log(props);
+
+  const [faq, setFaq] = useState([]);
+  useEffect(() => {
+    const FAQ_API_URL =
+      "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
     fetch(FAQ_API_URL)
-    .then((respostaDoServidor)=>{
-       return respostaDoServidor.json();
-    }).then((resposta) => setFaq(resposta))//console.log(resposta)
-  },[]);
+      .then((respostaDoServidor) => {
+        return respostaDoServidor.json();
+      })
+      .then((resposta) => {
+        setTimeout(() => {}, 5000);
+        setFaq(resposta);
+      }); //console.log(resposta)
+  }, []);
 
   return (
     <div>
       <h1>Alura Cases - Paginas de Perguntas FAQ</h1>
-      <Link href="/">
-        Ir para a Home
-      </Link>
+      <Link href="/">Ir para a Home</Link>
       <ul>
-        {faq.map(({answer,question})=> 
-          <li key={question}>{question}</li>
-         )} 
-        
+        {faq.map(({ answer, question }) => (
+          <li key={question}>
+            <h2>{question}</h2>
+            <p>{answer}</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
