f8195a6c31cca8a6de697c651af449eadca949f8
Marco <mbarozzi@gmail.com>
2024-09-15 15:17:42 -0300

    utilizando o getStaticProps

--- alura-cases-divulgacao/pages/faq.js
+++ alura-cases-divulgacao/pages/faq.js
@@ -2,31 +2,26 @@ import { useEffect, useState } from "react";
 import Link from "../scr/components/Link";
 
 export async function getStaticProps(context) {
+  const FAQ_API_URL =
+    "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
+
+  const faq = await fetch(FAQ_API_URL)
+    .then((respostaDoServidor) => {
+      return respostaDoServidor.json();
+    })
+    .then((resposta) => {
+      return resposta;
+    });
   return {
     props: {
-      texto: 'ainda esta em carga'
+      faq,
     },
   };
 }
 
-export default function FaqPage(props) {
-
-  console.log(props);
-
-  const [faq, setFaq] = useState([]);
-  useEffect(() => {
-    const FAQ_API_URL =
-      "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
-    fetch(FAQ_API_URL)
-      .then((respostaDoServidor) => {
-        return respostaDoServidor.json();
-      })
-      .then((resposta) => {
-        setTimeout(() => {}, 5000);
-        setFaq(resposta);
-      }); //console.log(resposta)
-  }, []);
-
+export default function FaqPage({faq}) {
+  console.log('faq ==>> '+faq);
+ 
   return (
     <div>
       <h1>Alura Cases - Paginas de Perguntas FAQ</h1>
