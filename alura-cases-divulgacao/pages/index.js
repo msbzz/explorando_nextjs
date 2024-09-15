2a6b4bdefb42d8f162299721e53e7552526df4c5
Marco <mbarozzi@gmail.com>
2024-09-15 03:52:30 -0300

    preparando estrutura pré definida

--- alura-cases-divulgacao/pages/index.js
+++ alura-cases-divulgacao/pages/index.js
@@ -1,13 +1,16 @@
-import Link from "../scr/components/Link";
-import Title from "../scr/components/titlle";
- 
- 
+// import Link from "../scr/components/Link";
+// import Footer from "../scr/components/patterns/Footer/footer";
+// import Title from "../scr/components/titlle";
 
-export default function HomePage() {
-  return (
-    <div>
-      <Title as='h1'>Alura Cases</Title>
-       <Link href="/faq">Ir para FAQ</Link>
-    </div>
-  );
-}
+import HomeScreen from "../scr/screens/HomeScreen";
+ 
+export default HomeScreen;
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <Title as='h1'>Alura Cases</Title>
+//        <Link href="/faq">Ir para FAQ</Link>
+//        <Footer/>
+//     </div>
+//   );
+// }
